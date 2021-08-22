a574ce981e59a87f393d71e571ba25e84a5d9baf
dvasylie <dvasylie@softserveinc.com>
2021-08-22 12:21:21 +0300

    prettier test

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,7 @@ const Card = ({ name, email, id }) => {
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
+            <div>div</div>
         <h2>{name}</h2>
         <p>{email}</p>
       </div>
